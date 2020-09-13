7689d219920221c2920c4df98a646b0998f82957
Donato Calvillo <edgar_donato@outlook.com>
2020-09-13 02:52:33 -0500

    Modelo ER y Tablas
    Creacion de Modelo Entidad-Relacion, y query con creacion de las tablas necesarias en MySql.

---
+++ MySql/Querys/Tablas.sql
@@ -0,0 +1,69 @@
+use CT_DB;
+
+/* No se si usaremos herencia o sera un atributo*/
+create table if not exists Tbl_Usuario(
+	email 		varchar(50) primary key not null,
+    username 	varchar(50) not null,
+    pass		varchar(50)	not null,
+    facebook	varchar(50) null,
+    twitter		varchar(50)	null,
+    imagen		varchar(50) null
+    /*rol	varchar(50) not null*/
+);
+create table if not exists Tbl_Noticia(
+	id_noticia	int auto_increment	primary key not null,
+	titulo		varchar(50) not null,
+    descripcion	varchar(100) not null,
+    noticia		varchar(1000) not null,
+    aprovado	bool default 0,
+    fk_usuario	varchar(50) not null,
+	foreign key (fk_usuario) references Tbl_Usuario(email)
+);
+
+create table if not exists Tbl_Categoria(
+	id_categoria	int auto_increment	primary key not null,
+    nombre			varchar(50) not null,
+    descripcion		varchar(200) null
+);
+
+create table if not exists Tbl_Not_Cat(
+	id_not_cat		int auto_increment	primary key not null,
+	fk_noticia		int not null,
+    fk_categoria	int not null,
+    foreign key (fk_noticia) references Tbl_Noticia(id_noticia),
+    foreign key (fk_categoria) references Tbl_Categoria(id_categoria)
+);
+
+create table if not exists Tbl_Imagen(
+	id_imagen	int auto_increment	primary key not null,
+    extencion	varchar(50) not null,
+    fk_noticia	int not null,
+	foreign key (fk_noticia) references Tbl_Noticia(id_noticia)
+);
+
+create table if not exists Tbl_Video(
+	id_video	int auto_increment	primary key not null,
+    ulr			varchar(50) not null,
+    fk_noticia	int not null,
+    foreign key (fk_noticia) references Tbl_Noticia(id_noticia)
+);
+
+create table if not exists Tbl_Comentario(
+	id_comentario	int auto_increment	primary key not null,
+    comentario		varchar(200) not null,
+	fk_usuario		varchar(50) not null,
+    fk_noticia		int not null,
+	foreign key (fk_usuario) references Tbl_Usuario(email),
+	foreign key (fk_noticia) references Tbl_Noticia(id_noticia)
+);
+
+create table if not exists Tbl_Guardadas(
+	id_guardadas	int auto_increment	primary key not null,
+    etiqueta		varchar(50) not null, /*Favoritos o ver mas tarde*/
+    fk_usuario		varchar(50) not null,
+	fk_noticia		int not null,
+    foreign key (fk_usuario) references Tbl_Usuario(email),
+    foreign key (fk_noticia) references Tbl_Noticia(id_noticia)
+);
+
+
