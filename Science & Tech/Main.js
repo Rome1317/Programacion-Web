0314ba7209661c0b7d8e9e45f2b18c480d7934c8
Rome1317 <rome_gs@hotmail.com>
2020-09-30 21:29:23 -0500

    Delete
    Borre carpetas

--- Science & Tech/Main.js
+++
@@ -1,3 +0,0 @@
-var navMenu = document.querySelector('#nav-menu');var btnExitMenu = document.querySelector('#btn-exit-menu');
-navMenu.addEventListener('click', ()=>{    var menu = document.querySelector('#menu');    menu.classList.toggle('menu-activo');});
-btnExitMenu.addEventListener('click', ()=>{    var menu = document.querySelector('#menu');    menu.classList.toggle('menu-activo');});
