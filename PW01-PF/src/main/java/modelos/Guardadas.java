baaa18ce7d40a69062457ed9f26441ea7a1f0f8e
Donato Calvillo <edgar_donato@outlook.com>
2020-10-18 00:00:05 -0500

    BACK END
    Todos los modelos con getters y setters

---
+++ PW01-PF/src/main/java/modelos/Guardadas.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+/**
+ *
+ * @author edgar
+ */
+public class Guardadas {
+//      ATRIBUTOS
+    private int id_guardadas;
+    private String etiqueta;
+    private String fk_usuario;
+    private int fk_noticia;
+    
+//    CONSTRUCTOR
+
+    public Guardadas(String etiqueta, String fk_usuario, int fk_noticia) {
+        this.etiqueta = etiqueta;
+        this.fk_usuario = fk_usuario;
+        this.fk_noticia = fk_noticia;
+    }
+
+// GETTER
+
+    public int getId_guardadas() {
+        return id_guardadas;
+    }
+
+    public String getEtiqueta() {
+        return etiqueta;
+    }
+
+    public String getFk_usuario() {
+        return fk_usuario;
+    }
+
+    public int getFk_noticia() {
+        return fk_noticia;
+    }
+    
+//    SETTER
+
+    public void setId_guardadas(int id_guardadas) {
+        this.id_guardadas = id_guardadas;
+    }
+
+    public void setEtiqueta(String etiqueta) {
+        this.etiqueta = etiqueta;
+    }
+
+    public void setFk_usuario(String fk_usuario) {
+        this.fk_usuario = fk_usuario;
+    }
+
+    public void setFk_noticia(int fk_noticia) {
+        this.fk_noticia = fk_noticia;
+    }
+    
+}
---
+++ PW01-PF/src/main/java/modelos/Imagen.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+/**
+ *
+ * @author edgar
+ */
+public class Imagen {
+//    ATRIBUTOS
+    private int id_imagen;
+    private String extencion;
+    private int fk_noticia;
+    
+//    CONSTRUCTOR
+
+    public Imagen(String extencion, int fk_noticia) {
+        this.extencion = extencion;
+        this.fk_noticia = fk_noticia;
+    }
+    
+//    GETTERS
+
+    public int getId_imagen() {
+        return id_imagen;
+    }
+
+    public String getExtencion() {
+        return extencion;
+    }
+
+    public int getFk_noticia() {
+        return fk_noticia;
+    }
+    
+//    SETTERS
+
+    public void setId_imagen(int id_imagen) {
+        this.id_imagen = id_imagen;
+    }
+
+    public void setExtencion(String extencion) {
+        this.extencion = extencion;
+    }
+
+    public void setFk_noticia(int fk_noticia) {
+        this.fk_noticia = fk_noticia;
+    }
+    
+    
+}
